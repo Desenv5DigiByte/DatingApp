f5e8778af271791cfe1c344ad2561c63e6ea41f9
Mateus Kern <desenv5.digibyte@outlook.com>
2019-05-06 13:39:29 -0300

    End of Section 5

--- DatingApp-SPA/src/app/services/auth.service.ts
+++ DatingApp-SPA/src/app/services/auth.service.ts
@@ -8,14 +8,14 @@ import { map } from 'rxjs/operators';
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model).pipe(
       map((response: any) => {
         const user = response;
         if (user) {
-          localStorage.setItem('token', user);
+          localStorage.setItem('token', user.token);
         }
       })
     );
@@ -24,5 +24,4 @@ export class AuthService {
   register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
   }
-
 }
